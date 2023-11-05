dbcc3b680487db4bb6f415265e08ec795cb59d73
Oliver030500 <112959885+Oliver030500@users.noreply.github.com>
2023-11-05 16:54:10 +0100

    First try at CompanyCard

---
+++ app/src/components/CompanyCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, Title, Paragraph, IconButton, MD3Colors } from 'react-native-paper';
+import { View, Image, StyleSheet,  } from 'react-native';
+
+const CompanyCard = () => (
+    <Card>
+        <Card.Content style={{ flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            backgroundColor: "#050506",
+            borderWidth:1,
+            borderColor:'grey',
+            borderRadius: 5,
+            alignItems: 'center',
+            paddingRight: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            height: 80,
+
+        }}>
+            <Title style={{color:'white',
+                           flex: 6,      }}>
+                INSERT_COMPANY_NAME
+            </Title>
+        </Card.Content>
+    </Card>
+);
+const styles = StyleSheet.create({
+});
+
+export default CompanyCard;
