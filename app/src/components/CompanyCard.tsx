8c270be77ba85b18b3ca197c6cc6bcf0010057cc
Oragif <miande22@student.aau.dk>
2023-11-06 12:53:35 +0100

    Fix this shit oliver

--- app/src/components/CompanyCard.tsx
+++ app/src/components/CompanyCard.tsx
@@ -1,30 +1,67 @@
-import React from 'react';
-import { Card, Title, Paragraph, IconButton, MD3Colors } from 'react-native-paper';
-import { View, Image, StyleSheet,  } from 'react-native';
+import React, { Component } from 'react';
+import { Card, IconButton, Title, TouchableRipple } from 'react-native-paper';
+import { View } from 'react-native';
 
-const CompanyCard = () => (
-    <Card>
-        <Card.Content style={{ flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            backgroundColor: "#050506",
-            borderWidth:1,
-            borderColor:'grey',
-            borderRadius: 5,
-            alignItems: 'center',
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            height: 80,
+enum Status {
+	NONE,
+	ACKNOWLEDGED,
+	ERROR
+}
 
-        }}>
-            <Title style={{color:'white',
-                           flex: 6,      }}>
-                INSERT_COMPANY_NAME
-            </Title>
-        </Card.Content>
-    </Card>
-);
-const styles = StyleSheet.create({
-});
+interface CompanyCardProps {
+	company: string;
+	state: Status;
+}
+
+interface CompanyCardState {
+	state: Status;
+}
+
+class CompanyCard extends Component<CompanyCardProps, CompanyCardState> {
+	state: CompanyCardState = {
+		state: Status.NONE
+	};
+
+	constructor(props: CompanyCardProps) {
+		super(props);
+		this.state.state = props.state;
+	}
+
+	private acknowledgeIconRender() {
+		return (
+			<>
+				<IconButton icon="account-check-outline" iconColor={'white'} size={25} style={[{ backgroundColor: '#E1B000' }]} />
+				<View style={{ backgroundColor: '#E1B000', width: 14, height: '100%' }} />
+			</>
+		);
+	}
+
+	render(): React.JSX.Element {
+		return (
+			<TouchableRipple onPress={() => console.log(1)} rippleColor={'rgba(255, 255, 255, 0.3)'}>
+				<Card>
+					<Card.Content
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'space-evenly',
+							backgroundColor: '#050506',
+							borderWidth: 1,
+							borderColor: 'grey',
+							borderRadius: 5,
+							alignItems: 'center',
+							paddingRight: 0,
+							paddingTop: 0,
+							paddingBottom: 0,
+							height: 80
+						}}
+					>
+						<Title style={{ color: 'white', flex: 6 }}>{this.props.company}</Title>
+						{this.state.state === Status.ACKNOWLEDGED ? this.acknowledgeIconRender() : null}
+					</Card.Content>
+				</Card>
+			</TouchableRipple>
+		);
+	}
+}
 
 export default CompanyCard;
--- app/src/components/CompanyCardAcknowledge.tsx
+++ app/src/components/CompanyCardAcknowledge.tsx
@@ -1,43 +1,28 @@
 import React from 'react';
 import { Card, Title, Paragraph, IconButton, MD3Colors } from 'react-native-paper';
-import { View, Image, StyleSheet,  } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 const CompanyCardAcknowledge = () => (
-    <Card>
-        <Card.Content style={{ flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            backgroundColor: "#050506",
-            borderWidth:1,
-            borderColor:'grey',
-            borderRadius: 5,
-            alignItems: 'center',
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            height: 80,
-
-        }}>
-            <Title style={{color:'white',
-                flex: 6,             }}>
-                INSERT_COMPANY_NAME
-            </Title>
-            <IconButton
-                icon="account-check-outline"
-                iconColor={'white'}
-                size={25}
-                style={[{backgroundColor: '#E1B000',
-                }]}
-            />
-            <View style={{backgroundColor:'#E1B000',
-                width:14,
-                height:'100%',
-            }}>
-
-            </View>
-        </Card.Content>
-    </Card>
+	<Card>
+		<Card.Content
+			style={{
+				flexDirection: 'row',
+				justifyContent: 'space-evenly',
+				backgroundColor: '#050506',
+				borderWidth: 1,
+				borderColor: 'grey',
+				borderRadius: 5,
+				alignItems: 'center',
+				paddingRight: 0,
+				paddingTop: 0,
+				paddingBottom: 0,
+				height: 80
+			}}
+		>
+			<Title style={{ color: 'white', flex: 6 }}>INSERT_COMPANY_NAME</Title>
+		</Card.Content>
+	</Card>
 );
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default CompanyCardAcknowledge;
