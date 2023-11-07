76f09828b6e1cf5b7960d3dae452633e6edd03b9
Oliver030500 <112959885+Oliver030500@users.noreply.github.com>
2023-11-07 20:43:49 +0100

    CompanyCard.tsx first draft for review

--- app/src/components/CompanyCard.tsx
+++ app/src/components/CompanyCard.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, IconButton, Title, TouchableRipple } from 'react-native-paper';
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
+import { getCurrentTheme } from '../themes/ThemeManager';
 
 enum Status {
 	NONE,
@@ -31,8 +32,40 @@ class CompanyCard extends Component<CompanyCardProps, CompanyCardState> {
 	private acknowledgeIconRender() {
 		return (
 			<>
-				<IconButton icon="account-check-outline" iconColor={'white'} size={25} style={[{ backgroundColor: '#E1B000' }]} />
-				<View style={{ backgroundColor: '#E1B000', width: 14, height: '100%' }} />
+				<IconButton icon="account-check-outline" iconColor={'white'} size={25} style={{ backgroundColor: '#E1B000' }} />
+				<View
+					style={{
+						backgroundColor: '#E1B000',
+						width: 14,
+						height: '100%',
+						borderTopRightRadius: 10,
+						borderBottomRightRadius: 10,
+						marginRight: 0.4
+					}}
+				/>
+			</>
+		);
+	}
+
+	private ErrorIconRender() {
+		return (
+			<>
+				<IconButton
+					icon="exclamation"
+					iconColor={getCurrentTheme().colors.onError}
+					size={25}
+					containerColor={getCurrentTheme().colors.error}
+				/>
+				<View
+					style={{
+						backgroundColor: getCurrentTheme().colors.error,
+						width: 14,
+						height: '100%',
+						borderTopRightRadius: 10,
+						borderBottomRightRadius: 10,
+						marginRight: 0.4
+					}}
+				/>
 			</>
 		);
 	}
@@ -40,24 +73,28 @@ class CompanyCard extends Component<CompanyCardProps, CompanyCardState> {
 	render(): React.JSX.Element {
 		return (
 			<TouchableRipple onPress={() => this.props.onPress()} rippleColor={'rgba(255, 255, 255, 0.3)'}>
-				<Card>
+				<Card
+					style={{
+						backgroundColor: getCurrentTheme().colors.surface,
+						borderWidth: 1,
+						borderColor: 'grey',
+						height: Dimensions.get('screen').height / 17
+					}}
+				>
 					<Card.Content
 						style={{
 							flexDirection: 'row',
 							justifyContent: 'space-evenly',
-							backgroundColor: '#050506',
-							borderWidth: 1,
-							borderColor: 'grey',
-							borderRadius: 5,
 							alignItems: 'center',
 							paddingRight: 0,
 							paddingTop: 0,
 							paddingBottom: 0,
-							height: 80
+							height: '100%'
 						}}
 					>
-						<Title style={{ color: 'white', flex: 6 }}>{this.props.company}</Title>
+						<Title style={{ color: getCurrentTheme().colors.onSurface, flex: 6 }}>{this.props.company}</Title>
 						{this.state.state === Status.ACKNOWLEDGED ? this.acknowledgeIconRender() : null}
+						{this.state.state === Status.ERROR ? this.ErrorIconRender() : null}
 					</Card.Content>
 				</Card>
 			</TouchableRipple>
