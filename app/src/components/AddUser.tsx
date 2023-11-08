3e8adea120a8523eff9c34b1de686b5af5ffe207
Oliver030500 <112959885+Oliver030500@users.noreply.github.com>
2023-11-08 20:06:14 +0100

    Writing's not easy. That's why Grammarly can help. This sentence is grammatically correct, but it's wordy, and hard to read. It undermines the writer's message and the word choice is bland. Grammarly's cutting edge technology helps you craft compelling, understandable writing that makes an impact on your reader. Much better. Are you ready to give it a try? Installation is simple and free. Visit Grammarly.com today!

--- app/src/components/AddUser.tsx
+++ app/src/components/AddUser.tsx
@@ -1,33 +1,107 @@
-import * as React from 'react';
-import { Button, Card, IconButton,Title } from 'react-native-paper';
+import React, { Component } from 'react';
+import { Card, IconButton, Portal, Text, Modal, Searchbar } from 'react-native-paper';
+import { getCurrentTheme } from '../themes/ThemeManager';
+import { StyleSheet, View } from 'react-native';
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: undefined,
+		position: 'absolute'
+	},
+	view: {
+		alignItems: 'center',
+		borderRadius: 20,
+		backgroundColor: getCurrentTheme().colors.surface,
+		width: 256,
+		height: 512
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%'
+	}
+});
 
+enum Status {
+	ASSIGNED,
+	CALLED
+}
 
-const AddUser = () => (
-    <Card>
-        <Card.Content style={{backgroundColor: '#141218',
-            borderWidth:1,
-            borderColor:'#938F99',
-            flexDirection:'row',
-            alignItems:'center',
-            height: 70,
-            borderRadius: 20,}}>
-            <Title style={{color:'white',
-                           fontSize:16,}}>
-                assigned
-            </Title>
+interface AssignUserProps {
+	visible: boolean;
+	onDismiss: () => void;
+}
 
+class AssignUser extends Component<AssignUserProps, Status> {
+	render(): React.JSX.Element {
+		return (
+			<Portal>
+				<Modal style={styles.container} visible={this.props.visible} onDismiss={() => this.props.onDismiss()}>
+					<View style={styles.view}>
+						<Searchbar placeholder={'User'} mode={'view'} icon={'account'} showDivider={true} />
+					</View>
+				</Modal>
+			</Portal>
+		);
+	}
+}
 
-            <IconButton
-                icon="plus-circle"
-                iconColor={'#CAC4D0'}
-                size={20}
-                onPress={() => console.log('Pressed')}
-                style={{borderRadius:100,
-                        borderWidth: 2,}}
-            />
-        </Card.Content>
-    </Card>
-);
+interface AddUserProps {
+	type: string;
+}
+
+class AddUser extends Component<AddUserProps> {
+	state = { assignVisible: false };
+
+	render(): React.JSX.Element {
+		return (
+			<Card
+				style={{
+					backgroundColor: getCurrentTheme().colors.surface,
+					borderWidth: 1,
+					borderColor: 'grey',
+					height: 45
+				}}
+			>
+				<Card.Content
+					style={{
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						paddingRight: 0,
+						paddingTop: 0,
+						paddingBottom: 0,
+						height: '100%'
+					}}
+				>
+					<Text
+						style={{
+							color: getCurrentTheme().colors.onSurface
+						}}
+					>
+						{this.props.type}
+					</Text>
+
+					<IconButton
+						icon="plus-circle"
+						iconColor={getCurrentTheme().colors.primary}
+						size={30}
+						onPress={() => this.setState({ assignVisible: true })}
+						style={{}}
+					/>
+					<AssignUser
+						visible={this.state.assignVisible}
+						onDismiss={() => {
+							this.setState({ assignVisible: false });
+						}}
+					></AssignUser>
+				</Card.Content>
+			</Card>
+		);
+	}
+}
 
 export default AddUser;
