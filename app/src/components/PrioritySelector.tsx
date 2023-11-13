53ebc9982b6a2e38e63cf9a08d024451f4cf0d00
Oragif <Mikkelhelsing1@gmail.com>
2023-11-13 15:54:08 +0100

    Fixed state

--- app/src/components/PrioritySelector.tsx
+++ app/src/components/PrioritySelector.tsx
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
 import { SegmentedButtons } from 'react-native-paper';
 
-import { getCurrentTheme } from '../themes/ThemeManager';
-
-enum Status {
-	'Priority1',
-	'Priority2',
-	'Priority3',
-	'Priority4'
-}
+type Status = 1 | 2 | 3 | 4;
 
 interface PrioritySelectorProps {
 	state: Status;
-	value: string;
-	onPress: () => void;
+	onPress: (value: number) => void;
 }
 interface PrioritySelectorState {
-	state: Status;
+	state: string;
 }
 class PrioritySelector extends Component<PrioritySelectorProps, PrioritySelectorState> {
 	state: PrioritySelectorState = {
-		state: Status.Priority3
+		state: '4'
 	};
 
 	render(): React.JSX.Element {
@@ -28,14 +20,15 @@ class PrioritySelector extends Component<PrioritySelectorProps, PrioritySelector
 			<SegmentedButtons
 				value={this.state.state}
 				density={'small'}
-				onValueChange={() => {
-					this.setState({ value: this.state.state });
+				onValueChange={(value) => {
+					this.setState({ state: value });
+					this.props.onPress(parseInt(value));
 				}}
 				buttons={[
-					{ value: this.state.state, label: 'P1' },
-					{ value: this.state.state, label: 'P2' },
-					{ value: this.state.state, label: 'P3' },
-					{ value: this.state.state, label: 'P4' }
+					{ value: '1', label: 'P1' },
+					{ value: '2', label: 'P2' },
+					{ value: '3', label: 'P3' },
+					{ value: '4', label: 'P4' }
 				]}
 			/>
 		);
