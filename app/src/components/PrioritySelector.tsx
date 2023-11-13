aba33f7ba7e6c1bff3b9ccee332f95ec92f704a1
Oliver030500 <112959885+Oliver030500@users.noreply.github.com>
2023-11-13 14:56:19 +0100

    Help me mikkel

--- app/src/components/PrioritySelector.tsx
+++ app/src/components/PrioritySelector.tsx
@@ -1,107 +1,45 @@
-import React, {Component} from 'react';
-import {Card, IconButton, Title, TouchableRipple} from 'react-native-paper';
-import {Dimensions, View} from 'react-native';
-import {getCurrentTheme} from '../themes/ThemeManager';
+import React, { Component } from 'react';
+import { SegmentedButtons } from 'react-native-paper';
+
+import { getCurrentTheme } from '../themes/ThemeManager';
 
 enum Status {
-    Priority1,
-    Priority2,
-    Priority3,
-    Priority4,
+	'Priority1',
+	'Priority2',
+	'Priority3',
+	'Priority4'
 }
 
 interface PrioritySelectorProps {
-    company: string;
-    state: Status;
-    onPress: () => void;
+	state: Status;
+	value: string;
+	onPress: () => void;
 }
-
 interface PrioritySelectorState {
-    state: Status;
+	state: Status;
 }
-
 class PrioritySelector extends Component<PrioritySelectorProps, PrioritySelectorState> {
-    state: PrioritySelectorState = {
-        state: Status.Priority4
-    };
+	state: PrioritySelectorState = {
+		state: Status.Priority3
+	};
 
-    constructor(props: PrioritySelectorProps) {
-        super(props);
-        this.state.state = props.state;
-    }
-
-    private acknowledgeIconRender() {
-        return (
-            <>
-                <IconButton icon="account-check-outline" iconColor={'white'} size={25}
-                            style={{backgroundColor: '#E1B000'}}/>
-                <View
-                    style={{
-                        backgroundColor: '#E1B000',
-                        width: 14,
-                        height: '100%',
-                        borderTopRightRadius: 10,
-                        borderBottomRightRadius: 10,
-                        marginRight: 0.4
-                    }}
-                />
-            </>
-        );
-    }
-
-    private ErrorIconRender() {
-        return (
-            <>
-                <IconButton
-                    icon="exclamation"
-                    iconColor={getCurrentTheme().colors.onError}
-                    size={25}
-                    containerColor={getCurrentTheme().colors.error}
-                />
-                <View
-                    style={{
-                        backgroundColor: getCurrentTheme().colors.error,
-                        width: 14,
-                        height: '100%',
-                        borderTopRightRadius: 10,
-                        borderBottomRightRadius: 10,
-                        marginRight: 0.4
-                    }}
-                />
-            </>
-        );
-    }
-
-    render(): React.JSX.Element {
-        return (
-            <TouchableRipple onPress={() => this.props.onPress()} rippleColor={'rgba(255, 255, 255, 0.3)'}>
-                <Card
-                    style={{
-                        backgroundColor: getCurrentTheme().colors.surface,
-                        borderWidth: 1,
-                        borderColor: 'grey',
-                        height: Dimensions.get('screen').height / 17
-                    }}
-                >
-                    <Card.Content
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-evenly',
-                            alignItems: 'center',
-                            paddingRight: 0,
-                            paddingTop: 0,
-                            paddingBottom: 0,
-                            height: '100%'
-                        }}
-                    >
-                        <Title style={{color: getCurrentTheme().colors.onSurface, flex: 6}}>{this.props.company}</Title>
-                        {this.state.state === Status.Priority1 ? this.acknowledgeIconRender() : null}
-                        {this.state.state === Status.Priority2 ? this.ErrorIconRender() : null}
-                    </Card.Content>
-                </Card>
-            </TouchableRipple>
-        );
-    }
+	render(): React.JSX.Element {
+		return (
+			<SegmentedButtons
+				value={this.state.state}
+				density={'small'}
+				onValueChange={() => {
+					this.setState({ value: this.state.state });
+				}}
+				buttons={[
+					{ value: this.state.state, label: 'P1' },
+					{ value: this.state.state, label: 'P2' },
+					{ value: this.state.state, label: 'P3' },
+					{ value: this.state.state, label: 'P4' }
+				]}
+			/>
+		);
+	}
 }
 
 export default PrioritySelector;
