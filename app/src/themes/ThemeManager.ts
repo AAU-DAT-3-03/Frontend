4a12b85d9b8c5cd8f17f173b79297446745547de
Oliver030500 <112959885+Oliver030500@users.noreply.github.com>
2023-11-16 09:18:00 +0100

    Hotfix For Colors: warn, error ,allGood

--- app/src/themes/ThemeManager.ts
+++ app/src/themes/ThemeManager.ts
@@ -1,8 +1,13 @@
 import { Appearance } from 'react-native';
-import { DarkTheme } from '../themes/DarkTheme';
-import { LightTheme } from '../themes/LightTheme';
+import { DarkTheme } from './DarkTheme';
+import { LightTheme } from './LightTheme';
 import { ThemeProp } from 'react-native-paper/lib/typescript/types';
 
+export const Colors = {
+	error: '#AA1E2E',
+	warn: '#E1B000',
+	allGood: '#44BE50'
+};
 /**
  * Gets the current theme. Light or dark depending on the system setting
  * @return {ThemeProp} - the current theme
