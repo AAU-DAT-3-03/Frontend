b9bf496271d8a6fb4d4921e7a2276b274f4f119a
Oliver030500 <112959885+Oliver030500@users.noreply.github.com>
2023-11-17 07:50:46 +0100

    gøjemøj bøvl tror vi skal have en tourturial i hvordan screens skal sættes op

--- app/src/screens/alarm/Alarm.tsx
+++ app/src/screens/alarm/Alarm.tsx
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import {Appbar, Text} from 'react-native-paper';
-import ContentContainer from "../../components/ContentContainer";
+import { Appbar } from 'react-native-paper';
+import ContentContainer from '../../components/ContentContainer';
+import InformationCard from '../../components/InformationCard';
+import NoteCard from '../../components/NoteCard';
 
 interface AlarmProps {
-	alarm: string
+	alarm: string;
+	alarmError: string;
+	alarmLog: string;
+	alarmNote: string;
+	service: string;
 }
 
-interface AlarmState {
-	alarm: string
-}
-
-
-class Alarm extends Component<AlarmProps, AlarmState> {
-	state: AlarmState = {
-		alarm: "Not defined"
+class AlarmInfo {
+	get service(): string {
+		return this._service as string;
 	}
 
+	set service(value: string) {
+		this._service = value;
+	}
+
+	get alarmNote(): string {
+		return this._alarmNote as string;
+	}
+
+	set alarmNote(value: string) {
+		this._alarmNote = value;
+	}
+
+	get alarmLog(): string {
+		return this._alarmLog as string;
+	}
+
+	set alarmLog(value: string) {
+		this._alarmLog = value;
+	}
+
+	get alarmError(): string {
+		return this._alarmError as string;
+	}
+
+	set alarmError(value: string) {
+		this._alarmError = value;
+	}
+
+	private _alarmError: string | undefined;
+	private _alarmLog: string | undefined;
+	private _alarmNote: string | undefined;
+	private _service: string | undefined;
+}
+interface AlarmState {
+	alarm: string;
+}
+
+class Alarm extends Component<AlarmProps, AlarmState, AlarmInfo> {
+	state: AlarmState = {
+		alarm: 'Not defined'
+	};
 	constructor(props: AlarmProps) {
 		super(props);
 		this.state.alarm = props.alarm;
@@ -24,8 +66,8 @@ class Alarm extends Component<AlarmProps, AlarmState> {
 	private AppBar(): React.JSX.Element {
 		return (
 			<Appbar>
-				<Appbar.BackAction onPress={() => {}}/>
-				<Appbar.Content title={this.state.alarm}/>
+				<Appbar.BackAction onPress={() => {}} />
+				<Appbar.Content title={this.state.alarm} />
 			</Appbar>
 		);
 	}
@@ -33,7 +75,8 @@ class Alarm extends Component<AlarmProps, AlarmState> {
 	render(): React.JSX.Element {
 		return (
 			<ContentContainer appBar={this.AppBar()}>
-				<Text variant={"displayLarge"}>test</Text>
+				<InformationCard errorType={this.state.alarm} errorInfo={this.props.alarmLog}></InformationCard>
+				<NoteCard noteInfo={this.props.alarmNote} onChange={() => {}}></NoteCard>
 			</ContentContainer>
 		);
 	}
