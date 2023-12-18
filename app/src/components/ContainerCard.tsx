324a2c551fa3509281fd0e44b924f773673fe41a
Oragif <miande22@student.aau.dk>
2023-12-18 21:25:05 +0100

    Container card comments and cleanup

--- app/src/components/ContainerCard.tsx
+++ app/src/components/ContainerCard.tsx
@@ -2,33 +2,55 @@ import React, { Component } from 'react';
 import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import { getCurrentTheme } from '../themes/ThemeManager';
 
+class HeaderRender extends Component<CardProps> {
+	render(): React.JSX.Element {
+		let style: StyleProp<ViewStyle>;
+
+		// Append the style prop if given
+		if (this.props.style !== undefined) {
+			style = { ...cardStyle().header, ...this.props.style };
+		} else {
+			style = cardStyle().header;
+		}
+
+		return <View style={style}>{this.props.children}</View>;
+	}
+}
+
+class ContentRender extends Component<CardProps> {
+	render(): React.JSX.Element {
+		return <View style={this.props.style}>{this.props.children}</View>;
+	}
+}
+
 interface CardProps {
 	children: React.ReactNode | React.ReactNode[] | undefined;
 	style?: StyleProp<any>;
 }
 
-class Header extends Component<CardProps> {
-	render(): React.JSX.Element {
-		return <View style={cardStyle().header}>{this.props.children}</View>;
-	}
-}
-
-class Content extends Component<CardProps> {
-	render(): React.JSX.Element {
-		return <View>{this.props.children}</View>;
-	}
-}
-
 class ContainerCard extends Component<CardProps> {
-	state = {
-		header: false,
-		content: false
-	};
-	static Header: (props: CardProps) => React.JSX.Element = (props: CardProps) => <Header style={props.style}>{props.children}</Header>;
-	static Content: (props: CardProps) => React.JSX.Element = (props: CardProps) => <Content style={props.style}>{props.children}</Content>;
+	/**
+	 * Header render for Container Card
+	 * @param {CardProps} props - Optional child elements and styling
+	 * @returns {React.JSX.Element} - The header render
+	 */
+	static Header: (props: CardProps) => React.JSX.Element = (props: CardProps): React.JSX.Element => (
+		<HeaderRender style={props.style}>{props.children}</HeaderRender>
+	);
+
+	/**
+	 * Content render for Container Card
+	 * @param {CardProps} props - Optional child elements and styling
+	 * @returns {React.JSX.Element} - The content render
+	 */
+	static Content: (props: CardProps) => React.JSX.Element = (props: CardProps): React.JSX.Element => (
+		<ContentRender style={props.style}>{props.children}</ContentRender>
+	);
 
 	render(): React.JSX.Element {
 		let style: StyleProp<ViewStyle>;
+
+		// Append the style prop if given
 		if (this.props.style !== undefined) {
 			style = { ...cardStyle().card, ...this.props.style };
 		} else {
