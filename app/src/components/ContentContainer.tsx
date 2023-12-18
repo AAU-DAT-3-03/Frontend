b163d1b4c7c1e6fbb0b3e19b2cc53ef1b7be8c2a
Oragif <miande22@student.aau.dk>
2023-12-18 21:30:52 +0100

    ContentContainer comments and cleanup

--- app/src/components/ContentContainer.tsx
+++ app/src/components/ContentContainer.tsx
@@ -1,11 +1,11 @@
 import React, { Component, ReactNode } from 'react';
-import { Appearance, RefreshControl, ScrollView, View } from 'react-native';
+import { Appearance, RefreshControl, ScrollView, StyleProp, View, ViewStyle } from 'react-native';
 import { getCurrentTheme } from '../themes/ThemeManager';
 import { Appbar } from 'react-native-paper';
 
 /**
  * If no appbar is  desired simply don't pass anything
- * Children will be collected automatically from within the html tags
+ * Children will be collected automatically from within the tags
  */
 interface ContentContainerProps {
 	appBar?: React.JSX.Element;
@@ -24,18 +24,21 @@ class ContentContainer extends Component<ContentContainerProps, ContentContainer
 
 	constructor(props: ContentContainerProps) {
 		super(props);
+		// Triggers when appearance of page changes, to ensure proper re-render
 		Appearance.addChangeListener(() => {
 			this.forceUpdate();
 		});
 	}
 
+	/**
+	 * Triggered once refresh of the page is finished, stops the refreshing animation
+	 */
 	private refreshFinished(): void {
 		this.setState({ refreshing: false });
 	}
 
 	/**
-	 * Calls the passed onRefresh
-	 * @private
+	 * Calls the passed onRefresh if given in props
 	 */
 	private onRefresh(): void {
 		if (this.props.onRefresh === undefined) return;
@@ -44,8 +47,8 @@ class ContentContainer extends Component<ContentContainerProps, ContentContainer
 	}
 
 	/**
-	 * Returns a refresh controller if onRefresh has been specified in the props
-	 * @private
+	 * Returns a refresh controller if onRefresh has been specified in the props.
+	 * Refresh controller handles the refresh animation, and starts it when pulling down on the page.
 	 */
 	private getRefreshControl(): React.JSX.Element | undefined {
 		if (this.props.onRefresh === undefined) return undefined;
@@ -53,8 +56,14 @@ class ContentContainer extends Component<ContentContainerProps, ContentContainer
 	}
 
 	render(): React.JSX.Element {
+		let wrapperStyle: StyleProp<ViewStyle> = {
+			flexDirection: 'column',
+			height: '100%',
+			backgroundColor: getCurrentTheme().colors.background
+		};
 		return (
-			<View style={{ flexDirection: 'column', height: '100%', backgroundColor: getCurrentTheme().colors.background }}>
+			<View style={wrapperStyle}>
+				{/* Render app bar if given in props */}
 				{this.props.appBar === undefined ? null : (
 					<Appbar style={{ backgroundColor: getCurrentTheme().colors.surface }}>{this.props.appBar}</Appbar>
 				)}
