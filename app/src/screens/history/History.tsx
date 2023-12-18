4b5a9249b080c2d1966dbe3f82de2bdf4bcc4dd6
Oragif <miande22@student.aau.dk>
2023-12-18 22:55:15 +0100

    History comments

--- app/src/screens/history/History.tsx
+++ app/src/screens/history/History.tsx
@@ -28,60 +28,93 @@ class History extends Component<any, HistoryState> {
 	state: HistoryState = {
 		loading: true,
 		query: '',
-		period: { start: getToday(-30), end: getToday() },
+		// Period defaults to the last 30 days from current day
+		period: {
+			start: getToday(-30),
+			end: getToday()
+		},
 		updating: false
 	};
 
 	constructor(props: any) {
 		super(props);
+		// Adds the render to the main app render for updating from other components
 		AppRender.history = this;
 	}
 
+	/**
+	 * Creates the app bar render
+	 * @returns {React.JSX.Element} - Returns the app bar render with a date selector searchbar
+	 */
 	private AppBar(): React.JSX.Element {
 		return (
-			<>
-				<Appbar.Header style={{ backgroundColor: getCurrentTheme().colors.surface }}>
-					<SearchBarDateSelector
-						onChange={(query: string, period: Period) => {
-							if (
-								!compareDatesEqual(this.state.period.start, period.start) ||
-								!compareDatesEqual(this.state.period.end, period.end)
-							) {
-								this.setState({ updating: true }, () => {
-									this.getIncidentResponse(period);
-								});
-							}
-							this.setState({ period: period, query: query.toLowerCase() });
-						}}
-					/>
-				</Appbar.Header>
-			</>
+			<Appbar.Header style={HistoryStyle().appBarHeader}>
+				<SearchBarDateSelector onChange={(query: string, period: Period) => this.onChange(query, period)} />
+			</Appbar.Header>
 		);
 	}
 
+	/**
+	 * Handles the onChange of the searchbar from the appBar
+	 * @param {string} query - Search query
+	 * @param {Period} period - Start and end date
+	 */
+	private onChange(query: string, period: Period): void {
+		// If either of the dates has changed get new data from server
+		let startDataChanged: boolean = !compareDatesEqual(this.state.period.start, period.start);
+		let endDataChanged: boolean = !compareDatesEqual(this.state.period.end, period.end);
+		if (startDataChanged || endDataChanged) {
+			this.setState({ updating: true }, (): void => {
+				this.getIncidentResponse(period);
+			});
+		}
+
+		this.setState({ period: period, query: query.toLowerCase() });
+	}
+
+	/**
+	 * Refreshes data on the history screen, called from other components
+	 */
 	public refresh(): void {
 		this.setState({ updating: true }, () => {
 			this.getIncidentResponse(this.state.period);
 		});
 	}
 
+	/**
+	 * Gets all resolved incidents within a period
+	 * @param {Period} period - The period to get data from
+	 */
 	private async getIncidentResponse(period: Period): Promise<void> {
+		// Convert period to iso-8601 standard (Milliseconds since 1/1/1970)
 		let start: number = new Date(period.start[2], period.start[1] - 1, period.start[0]).getTime();
+		// 86399999 sets the time to a millisecond before midnight
 		let end: number = new Date(period.end[2], period.end[1] - 1, period.end[0]).getTime() + 86399999;
+
+		// Get the data from the specified period
 		this.logger.info(`Getting incident date for period: ${start}-${end} - ${new Date(start)}-${new Date(end)}`);
 		let incidentData: IncidentResponse[] = await DataHandler.getResolvedIncidentsData(start, end);
+
 		this.logger.info('Rendering Incidents');
+		// Update the state and data of the history screen
 		this.incidents = incidentData;
 		this.setState({ loading: false, updating: false });
 	}
 
+	/**
+	 * Get resolved data from the server when the component gets mounted
+	 */
 	componentDidMount() {
 		this.getIncidentResponse(this.state.period);
 	}
 
+	/**
+	 * Filters the incidents based on the query from the searchbar
+	 * @param {IncidentResponse} incidentData
+	 */
 	private filterIncidentList(incidentData: IncidentResponse[] | undefined): IncidentResponse[] | undefined {
 		if (incidentData !== undefined) {
-			incidentData = incidentData.filter((incident) => {
+			incidentData = incidentData.filter((incident: IncidentResponse) => {
 				return filterIncidentList(incident, this.state.query);
 			});
 		}
@@ -89,12 +122,21 @@ class History extends Component<any, HistoryState> {
 		return incidentData;
 	}
 
+	/**
+	 * Creates the Incident Card render
+	 * @param {ListRenderItemInfo<IncidentResponse>} info - The incident to create an incident card render for
+	 * @returns {React.JSX.Element} - Incident Card render
+	 */
 	private incidentCardRender(info: ListRenderItemInfo<IncidentResponse>): React.JSX.Element {
 		let navigation = this.props.navigation;
+
+		// Handles navigation when clicking on the incident
 		let onClickIncident = (id: string) =>
 			navigation.navigate('Incident', {
 				id: id
 			});
+
+		// Handles navigation when clicking on an alarm
 		let onClickAlarm = (id: string, alarm: AlarmResponse) =>
 			navigation.navigate('Alarm', {
 				id: id,
@@ -103,14 +145,16 @@ class History extends Component<any, HistoryState> {
 		return <IncidentCard incident={info.item} onClickIncident={onClickIncident.bind(this)} onClickAlarm={onClickAlarm.bind(this)} />;
 	}
 
-	private incidentsRender(): React.JSX.Element {
-		let incidentData = this.filterIncidentList(this.incidents);
+	/**
+	 * Creates the render for the list of incidents
+	 * @returns {React.JSX.Element} - The incident list render
+	 */
+	private incidentListRender(): React.JSX.Element {
+		// Filter the list of incidents
+		let incidentData: IncidentResponse[] | undefined = this.filterIncidentList(this.incidents);
 		return (
-			<ScrollView
-				horizontal={true}
-				showsVerticalScrollIndicator={false}
-				contentContainerStyle={{ width: '100%', height: '100%', margin: 0, padding: 0 }}
-			>
+			<ScrollView horizontal={true} showsVerticalScrollIndicator={false} contentContainerStyle={HistoryStyle().incidentList}>
+				{/* Flat list is virtualized render, which renders items in batches to minimize lag */}
 				<FlatList
 					data={incidentData}
 					showsVerticalScrollIndicator={false}
@@ -122,6 +166,10 @@ class History extends Component<any, HistoryState> {
 		);
 	}
 
+	/**
+	 * Creates the render for loading initial data or no incidents screen if none is available
+	 * @returns {React.JSX.Element} - Either the loading screen render or no active screen render
+	 */
 	private noIncidentsRender(): React.JSX.Element {
 		return (
 			<>
@@ -136,6 +184,10 @@ class History extends Component<any, HistoryState> {
 		);
 	}
 
+	/**
+	 * Handles the refresh of the screen, when user pulls down
+	 * @param {() => void} finished - Function to call when refresh is finished
+	 */
 	private onRefresh(finished: () => void): void {
 		this.getIncidentResponse(this.state.period).then(() => finished());
 	}
@@ -146,7 +198,7 @@ class History extends Component<any, HistoryState> {
 				<LoadingIcon visible={this.state.updating} verticalOffset={60} />
 
 				<ContentContainer appBar={this.AppBar()} onRefresh={(finished) => this.onRefresh(finished)}>
-					{this.incidents === undefined ? this.noIncidentsRender() : this.incidentsRender()}
+					{this.incidents === undefined ? this.noIncidentsRender() : this.incidentListRender()}
 				</ContentContainer>
 			</View>
 		);
@@ -166,6 +218,15 @@ const HistoryStyle = () => {
 			padding: 16,
 			flexDirection: 'column',
 			gap: 16
+		},
+		appBarHeader: {
+			backgroundColor: getCurrentTheme().colors.surface
+		},
+		incidentList: {
+			width: '100%',
+			height: '100%',
+			margin: 0,
+			padding: 0
 		}
 	});
 };
